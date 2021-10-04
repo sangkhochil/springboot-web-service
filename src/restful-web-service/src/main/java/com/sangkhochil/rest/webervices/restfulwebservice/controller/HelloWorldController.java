71492a3ddbab5ffe3cdb15273c098eacfc50d16c
Jahangir Alam <ja.dakghor@gmail.com>
2021-10-04 12:49:12 +0600

    added swagger
    Signed-off-by: Jahangir Alam <ja.dakghor@gmail.com>

--- src/restful-web-service/src/main/java/com/sangkhochil/rest/webervices/restfulwebservice/controller/HelloWorldController.java
+++ src/restful-web-service/src/main/java/com/sangkhochil/rest/webervices/restfulwebservice/controller/HelloWorldController.java
@@ -1,15 +1,24 @@
 package com.sangkhochil.rest.webervices.restfulwebservice.controller;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContext;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sangkhochil.rest.webervices.restfulwebservice.common.Utils;
 import com.sangkhochil.rest.webervices.restfulwebservice.model.HelloWorldBean;
 
 @RestController
 public class HelloWorldController {
+	
+	@Autowired
+	MessageSource messageSource;
 
 	//@RequestMapping(method = RequestMethod.GET, path = "/hello-world")
 	@GetMapping(path = "/hello-world") //-----> shorter form for specific request methods
@@ -25,5 +34,19 @@ public class HelloWorldController {
 	@GetMapping(path = "/hello-world/{name}")
 	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
 		return new HelloWorldBean(String.format("Hello-World: %s", name));
+	}
+	
+	@GetMapping(path = "/hello-world-internationalized")
+	public String helloWorldInternationalized(
+			//@RequestHeader( name = "Accept-Language", required = false) Locale locale
+			) {
+		
+		//return "Hello-World";
+		//en = Hello World
+		//nl = Goede Morgen
+		//fr = Bonjour
+		
+		//return messageSource.getMessage(Utils.GOOD_MORNING_MESSAGE, null, "Default Message", locale);
+		return messageSource.getMessage(Utils.GOOD_MORNING_MESSAGE, null, LocaleContextHolder.getLocale()); // if client not select Accept-Language, server return default english message
 	}
 }
