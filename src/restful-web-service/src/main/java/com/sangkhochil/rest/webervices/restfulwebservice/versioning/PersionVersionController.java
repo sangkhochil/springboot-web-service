a4c563ed511ff48d4670b2f5fe4bc557fb7a1747
Jahangir Alam <ja.dakghor@gmail.com>
2021-10-04 17:13:40 +0600

    rest api versioning by params
    Signed-off-by: Jahangir Alam <ja.dakghor@gmail.com>

--- src/restful-web-service/src/main/java/com/sangkhochil/rest/webervices/restfulwebservice/versioning/PersionVersionController.java
+++ src/restful-web-service/src/main/java/com/sangkhochil/rest/webervices/restfulwebservice/versioning/PersionVersionController.java
@@ -14,4 +14,14 @@ public class PersionVersionController {
 	public PersonV2 getPersionV2() {
 		return new PersonV2(new Name("Charlin", "Chaplen"));
 	}
+	
+	@GetMapping(path="/person", params = "version=1")
+	public PersonV1 paramV1() {
+		return new PersonV1("Charlin Chaplen");
+	}
+	
+	@GetMapping(path="/person", params = "version=2")
+	public PersonV2 paramV2() {
+		return new PersonV2(new Name("Charlin", "Chaplen"));
+	}
 }
