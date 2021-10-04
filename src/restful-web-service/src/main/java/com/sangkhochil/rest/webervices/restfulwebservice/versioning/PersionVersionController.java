a7f276f8848fefec313d7e6ecfc245103ffab43e
Jahangir Alam <ja.dakghor@gmail.com>
2021-10-04 18:35:58 +0600

    rest api versioning by producers
    Signed-off-by: Jahangir Alam <ja.dakghor@gmail.com>

--- src/restful-web-service/src/main/java/com/sangkhochil/rest/webervices/restfulwebservice/versioning/PersionVersionController.java
+++ src/restful-web-service/src/main/java/com/sangkhochil/rest/webervices/restfulwebservice/versioning/PersionVersionController.java
@@ -5,33 +5,56 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PersionVersionController {
+	
+	//
+	//URI based versioning
+	//
 	@GetMapping(path="v1/person")
 	public PersonV1 getPersionV1() {
-		return new PersonV1("Charlin Chaplen");
+		return new PersonV1("Charlie Chaplen");
 	}
 	
 	@GetMapping(path="v2/person")
 	public PersonV2 getPersionV2() {
-		return new PersonV2(new Name("Charlin", "Chaplen"));
+		return new PersonV2(new Name("Charlie", "Chaplen"));
 	}
 	
-	@GetMapping(path="/person", params = "version=1")
+	//
+	//URI param versioning
+	//
+	@GetMapping(path="/person/params", params = "version=1")
 	public PersonV1 paramV1() {
-		return new PersonV1("Charlin Chaplen");
+		return new PersonV1("Charlie Chaplen");
 	}
 	
-	@GetMapping(path="/person", params = "version=2")
+	@GetMapping(path="/person/params", params = "version=2")
 	public PersonV2 paramV2() {
-		return new PersonV2(new Name("Charlin", "Chaplen"));
+		return new PersonV2(new Name("Charlie", "Chaplen"));
 	}
 	
-	@GetMapping(path="/person",headers = "X_VERSION=1")
+	//
+	//header versioning
+	//
+	@GetMapping(path="/person/headers", headers = "X_VERSION=1")
 	public PersonV1 headerV1() {
-		return new PersonV1("Charlin Chaplen");
+		return new PersonV1("Charlie Chaplen");
 	}
 	
-	@GetMapping(path="/person", headers = "X_VERSION=2")
+	@GetMapping(path="/person/headers", headers = "X_VERSION=2")
 	public PersonV2 headerV2() {
-		return new PersonV2(new Name("Charlin", "Chaplen"));
+		return new PersonV2(new Name("Charlie", "Chaplen"));
+	}
+	
+	//
+	//header accept versioning
+	//
+	@GetMapping(path="/person/producers", produces = "application/com.sangkhochil.app-v1+json")
+	public PersonV1 producerV1() {
+		return new PersonV1("Charlie Chaplen");
+	}
+	
+	@GetMapping(path="/person/producers",  produces = "application/com.sangkhochil.app-v2+json")
+	public PersonV2 producerV2() {
+		return new PersonV2(new Name("Charlie", "Chaplen"));
 	}
 }
