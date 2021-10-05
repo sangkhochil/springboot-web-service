cd0a5654a4903d75aa15f765364ce948a59d6737
Jahangir Alam <ja.dakghor@gmail.com>
2021-10-05 08:51:37 +0600

    basic h2 database configuration
    Signed-off-by: Jahangir Alam <ja.dakghor@gmail.com>

--- src/restful-web-service/src/main/java/com/sangkhochil/rest/webervices/restfulwebservice/model/User.java
+++ src/restful-web-service/src/main/java/com/sangkhochil/rest/webervices/restfulwebservice/model/User.java
@@ -1,11 +1,16 @@
 package com.sangkhochil.rest.webervices.restfulwebservice.model;
 
 import java.util.Date;
-
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
+@Entity
 public class User {
+	@Id
+	@GeneratedValue
 	private Integer id;
 
 	@Size(min = 2, max = 10, message = "Name should be between 2 to 10 characters")
---
+++ src/restful-web-service/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into user values(1, sysdate(), 'AB')
+insert into user values(2, sysdate(), 'Michael')
+insert into user values(3, sysdate(), 'Jackson')
